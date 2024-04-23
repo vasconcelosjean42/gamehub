43f74b61e17f2fc33c80ee33fa2baddc69287584
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2024-04-23 19:19:58 -0300

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = new Array(10).fill(0).map((_, i) => i);
 
   return (
     <>
@@ -13,6 +15,8 @@ const GameGrid = () => {
         spacing={10}
         padding={"10px"}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id.toString()} game={game}></GameCard>
         ))}
